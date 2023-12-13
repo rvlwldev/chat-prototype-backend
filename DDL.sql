d770795c05ced2bf9aef5d50db5935ee9fbf8ccf
rvlwldev <rvlwlasd77@gmail.com>
2023-12-13 15:42:00 +0900

    fix: ddl 수정

--- DDL.sql
+++ DDL.sql
@@ -1,86 +1,49 @@
+-- chat.CHANNEL definition
+
 CREATE TABLE `CHANNEL` (
-  `id` varchar(50) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `type` varchar(15) NOT NULL,
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '채널 ID',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '채널명',
+  `type` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '채널타입',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- chat.MESSAGE definition
 
 CREATE TABLE `MESSAGE` (
-  `id` varchar(50) NOT NULL,
-  `parentMessageId`  varchar(50),
-  `channelId` varchar(50) NOT NULL,
-  `userId` varchar(50) NOT NULL,
-  `text` text ,
-  `type` varchar(10) NOT NULL DEFAULT 'text',
-  `filePath` text DEFAULT NULL,
-  `fileName` text DEFAULT NULL,
-  `fileSize` varchar(50) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'ID',
+  `parentMessageId` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '부모 ID',
+  `channelId` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '채널 ID',
+  `userId` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '유저 ID',
+  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '메세지 텍스트',
+  `type` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'text' COMMENT '메세지 타입',
+  `filePath` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '첨부파일 경로',
+  `fileName` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '첨부파일 이름',
+  `fileSize` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '첨부파일 크기',
+  `createdAt` bigint NOT NULL COMMENT '생성일자',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- chat.`USER` definition
 
 CREATE TABLE `USER` (
-  `id` varchar(50)  NOT NULL,
-  `username` varchar(20)  NOT NULL,
-  `profileUserImageUrl` text,
-  `role` int default 1,
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '유저 ID',
+  `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '유저명',
+  `profileUserImageUrl` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '프로필사진 경로',
+  `role` int DEFAULT '1' COMMENT '권한',
+  `temp_pw` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- chat.USER_CHANNEL definition
 
 CREATE TABLE `USER_CHANNEL` (
-  `userId` varchar(50) NOT NULL,
-  `channelId` varchar(50) NOT NULL,
-  PRIMARY KEY (`userId`, `channelId`),
-  FOREIGN KEY (`userId`) REFERENCES `USER`(`id`),
-  FOREIGN KEY (`channelId`) REFERENCES `CHANNEL`(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-ALTER TABLE `MESSAGE`
-ADD CONSTRAINT `FK_CHANNEL_ID`
-FOREIGN KEY (`channelId`)
-REFERENCES `CHANNEL`(`id`);
-
-ALTER TABLE `MESSAGE`
-ADD CONSTRAINT `FK_USER_ID`
-FOREIGN KEY (`userId`)
-REFERENCES `USER`(`id`);
-
-ALTER TABLE `MESSAGE`
-ADD CONSTRAINT `FK_PARENT_MESSAGE_ID`
-FOREIGN KEY (`parentMessageId`)
-REFERENCES `MESSAGE`(`id`);
-
-INSERT INTO chat.`USER` (id,username,profileUserImageUrl,`role`) VALUES
-	 ('44c5a8a4384857c6','강수면','',1),
-	 ('47e2beb243c5bb9c','김형준(전산)','',1),
-	 ('admin01','관리자01',NULL,99),
-	 ('admin02','관리자02',NULL,99),
-	 ('admin03','관리자03',NULL,99),
-	 ('admin04','관리자04',NULL,99),
-	 ('admin05','관리자05',NULL,99),
-	 ('admin06','관리자06',NULL,99),
-	 ('admin07','관리자07',NULL,99),
-	 ('admin08','관리자08',NULL,99);
-INSERT INTO chat.`USER` (id,username,profileUserImageUrl,`role`) VALUES
-	 ('admin09','관리자09',NULL,99),
-	 ('admin10','관리자10',NULL,99);
-
-
- SELECT M.id,
-             M.parentMessageId,
-             M.channelId,
-             M.userId,
-             U.username
-             U.profileUserImageUrl AS profileImageUrl,
-             M.text,
-             M.type,
-             M.filePath,
-             M.fileName,
-             M.fileSize,
-             M.createdAt
-        FROM MESSAGE M
-        LEFT
-       OUTER
-        JOIN USER U
-          ON M.userId = U.id
+  `userId` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '유저 ID',
+  `channelId` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '채널 ID',
+  PRIMARY KEY (`userId`,`channelId`),
+  KEY `channelId` (`channelId`),
+  CONSTRAINT `USER_CHANNEL_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `USER` (`id`),
+  CONSTRAINT `USER_CHANNEL_ibfk_2` FOREIGN KEY (`channelId`) REFERENCES `CHANNEL` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
