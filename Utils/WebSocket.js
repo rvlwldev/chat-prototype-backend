b75427b1f3576d6db24adbddc419a01c97ae159f
rvlwldev <rvlwlasd77@gmail.com>
2023-12-21 11:50:00 +0900

    init: webSocket 초기화

--- Utils/WebSocket.js
+++ Utils/WebSocket.js
@@ -1,49 +1,100 @@
 const WebSocket = require("ws");
 
-/** @type WebSocket */
-var WS;
+class ClientConnection {
+	#ID;
 
-function initializeWebSocket(server) {
-	const webSocketServer = new WebSocket.Server({ server });
+	channelIdSet = new Set();
+	/** @type WebSocket.Server */ WS;
 
-	webSocketServer.on("connection", (ws, req) => {
-		ws.on("message", (payload) => {
-			try {
-				const PAYLOAD = JSON.parse(payload);
-				let event = PAYLOAD.event;
+	constructor(serviceId, userId, ws) {
+		this.#ID = `${serviceId}-${userId}`;
+		this.WS = ws;
+	}
 
-				switch (event) {
-					case "subscribe-channel":
-						listenChannelMessages(PAYLOAD.channelId);
-						break;
-					case "read":
-						break;
-				}
-			} catch (error) {
-				console.error("메시지 파싱 오류:", error);
-				console.log(payload);
-			}
-		});
+	addChannelId = (channelId) => this.channelIdSet.add(channelId);
+	deleteChannelId = (channelId) => this.channelIdSet.delete(channelId);
 
-		ws.on("error", (err) => {
-			console.error("WebSocket Error");
-			console.error(err);
-		});
+	valueOf = () => this.#ID;
+}
 
-		ws.on("close", () => {
-			console.log("클라이언트 접속 해제");
-		});
+/** @type Map<String, ClientConnection> */
+const connectionMap = new Map();
 
-		ws.on("test", (str) => {
-			console.log("test String : " + str);
-		});
+function openSocketWithServer(server) {
+	const wss = new WebSocket.Server({ server });
+
+	const getParams = (url) => {
+		const params = new URLSearchParams(url.split("?")[1]);
+		return {
+			serviceId: params.get("serviceId"),
+			userId: params.get("userId"),
+		};
+	};
+
+	wss.on("connection", (ws, req) => {
+		const { serviceId, userId } = getParams(req.url);
+		ws.onclose = () => removeClient(serviceId, userId);
+
+		// TODO: validation
+		if (!validateConnection(serviceId, userId)) {
+			// TODO : 실패 메세지 보내기
+			ws.terminate();
+			return;
+		}
+
+		// 클라이언트 에러
+		ws.onerror = (error) => console.error(`클라이언트 에러: ${error.message}`);
+
+		const connection = new ClientConnection(serviceId, userId, ws);
+		connectionMap.set(`${serviceId}-${userId}`, connection);
 	});
 }
 
-// TODO : 특정 채널의 메세지 올라오는거 대기 타기
-function subscribeChannelMessages(channelId) {}
+function validateConnection(serviceId, userId) {
+	return true;
+}
 
-// TODO : 특정 채널의 읽음처리
-function channelRead(channelId) {}
+function removeClient(serviceId, userId) {
+	const ID = `${serviceId}-${userId}`;
+	if (!connectionMap.has(ID)) return;
 
-module.exports = { initializeWebSocket, WS };
+	connectionMap.get(ID).WS.terminate();
+	connectionMap.delete(ID);
+}
+
+async function sendMessage(ws, eventName, message) {
+	return new Promise((resolve, reject) => {
+		ws.send(
+			JSON.stringify({
+				event: eventName,
+				message: message,
+			}),
+			(error) => {
+				if (error) reject(error);
+				else resolve();
+			}
+		);
+	});
+}
+
+const WS = {
+	subscribe: (serviceId, userId, channelId) => {
+		const connection = connectionMap.get(`${serviceId}-${userId}`);
+
+		if (!connection) {
+			// TODO : 클라이언트가 없으면 예외처리
+			return;
+		}
+
+		connection.addChannelId(channelId);
+	},
+
+	publish: async (channelId, eventName, message) => {
+		for (const connection of connectionMap.keys()) {
+			if (connection.channelIdSet.has(channelId))
+				sendMessage(connection.WS, eventName, message);
+		}
+	},
+};
+
+module.exports = { openSocketWithServer, WS };
