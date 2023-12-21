75d1f41b4de019dc3fe702459b76b079d3363015
rvlwldev <rvlwlasd77@gmail.com>
2023-12-21 15:38:28 +0900

    feat: ChannelController - save 구현

--- Controller/Channel.js
+++ Controller/Channel.js
@@ -3,22 +3,38 @@ const prisma = require("../Utils/Prisma");
 const ServiceController = require("./Service");
 const UserController = require("./User");
 
-const ChannelController = {
-	getChannelsByUserId: async (serviceId, userId) => {
-		const SERVICE = await ServiceController.getService(serviceId);
-		const USER = await UserController.getUser(serviceId, userId);
+const { v4: uuidv4 } = require("uuid");
 
-		const USER_CHANNELS = await prisma.userChannel
+const ChannelController = {
+	getChannelsByUserId: async (serviceId, userId) =>
+		await prisma.userChannel
 			.findMany({
 				where: {
-					Service: SERVICE,
-					user: USER,
+					Service: await ServiceController.getService(serviceId),
+					user: await UserController.getUser(serviceId, userId),
 				},
 				include: { channel: true },
 			})
-			.finally(() => prisma.$disconnect());
+			.then((userChannels) => userChannels.map((userChannel) => userChannel.channel))
+			.finally(() => prisma.$disconnect()),
 
-		return USER_CHANNELS.map((userChannel) => userChannel.channel);
+	// TODO : 채널타입 모델링
+	saveChannel: async (serviceId, type, name, createUserId) => {
+		return await prisma.channel
+			.create({
+				data: {
+					service: await prisma.service.findUnique({ where: { id: serviceId } }),
+					user: await prisma.user.findUnique(createUserId),
+
+					id: uuidv4(),
+					type: type,
+					name: name,
+				},
+			})
+			.catch((err) => {
+				throw err;
+			})
+			.finally(() => prisma.$disconnect());
 	},
 };
 
