544cc5aaebb38d06aacfa7ece96b71b4ab45574b
rvlwldev <rvlwlasd77@gmail.com>
2023-12-26 11:14:35 +0900

    feat: ChannelException 모듈 구현 및 예외처리

--- Exception/Channel/ChannelNotFound.js
+++
@@ -1,8 +0,0 @@
-class ChannelNotFoundException extends Error {
-	constructor(channelId) {
-		super(`채널 ID를 찾을 수 없습니다.\nID : ${channelId}`);
-		this.name = "ChannelNotFoundException";
-	}
-}
-
-module.exports = ChannelNotFoundException;
