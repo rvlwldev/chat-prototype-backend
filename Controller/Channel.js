04176038f9a5cd55b9fdcd583a646f1b36c87c05
rvlwldev <rvlwlasd77@gmail.com>
2023-12-26 11:14:22 +0900

    feat: channelController 구현

--- Controller/Channel.js
+++ Controller/Channel.js
@@ -1,3 +1,5 @@
+const { channel } = require("diagnostics_channel");
+const ChannelException = require("../Exception/ChannelException");
 const prisma = require("../Utils/Prisma");
 
 const ServiceController = require("./Service");
@@ -5,36 +7,115 @@ const UserController = require("./User");
 
 const { v4: uuidv4 } = require("uuid");
 
+// TODO : param 수정
 const ChannelController = {
-	getChannelsByUserId: async (serviceId, userId) =>
-		await prisma.userChannel
-			.findMany({
-				where: {
-					Service: await ServiceController.getService(serviceId),
-					user: await UserController.getUser(serviceId, userId),
-				},
-				include: { channel: true },
+	getChannelById: async (channelId) =>
+		await prisma.channel
+			.findUnique({ where: { id: channelId } })
+			.catch((err) => {
+				if (err.code == "P2020") throw new ChannelException.NotFound();
 			})
-			.then((userChannels) => userChannels.map((userChannel) => userChannel.channel))
 			.finally(() => prisma.$disconnect()),
 
-	// TODO : 채널타입 모델링
-	saveChannel: async (serviceId, type, name, createUserId) => {
-		return await prisma.channel
+	getChannelsByUser: async (SERVICE, USER) =>
+		await prisma.userChannel
+			.findMany({
+				where: { Service: SERVICE, user: USER },
+				select: { channel: true },
+			})
+			.finally(() => prisma.$disconnect()),
+
+	saveChannel: async (SERVICE, USER, type, name) => {
+		// TODO : 채널타입 모델링
+		if (type.includes("public")) {
+			if (USER.role.id < 9) throw new ChannelException.NotAllowed();
+			else if (!name) throw new ChannelException.NameRequired();
+
+			let where = { serviceId: USER.serviceId, type: type, name: name };
+			await prisma.channel.findFirst({ where: where }).then((channel) => {
+				if (channel) throw new ChannelException.Duplicated();
+			});
+		}
+
+		let channelId = uuidv4().substring(0, 12);
+
+		const newChannel = await prisma.channel.create({
+			data: {
+				service: SERVICE,
+				id: channelId,
+				name: name,
+				type: type,
+			},
+			select: {
+				service: false,
+				id: true,
+				name: true,
+				type: true,
+			},
+		});
+
+		await prisma.userChannel.create({ data: { channel: newChannel, user: USER } });
+
+		return newChannel;
+	},
+
+	saveUserChannel: async (SERVICE, CHANNEL, USER) =>
+		await prisma.userChannel
 			.create({
 				data: {
-					service: await prisma.service.findUnique({ where: { id: serviceId } }),
-					user: await prisma.user.findUnique(createUserId),
-
-					id: uuidv4(),
-					type: type,
-					name: name,
+					service: SERVICE,
+					channel: CHANNEL,
+					user: USER,
 				},
 			})
 			.catch((err) => {
-				throw err;
+				console.log("saveUserChannel ERROR");
+				console.log(err);
 			})
-			.finally(() => prisma.$disconnect());
+			.finally(() => prisma.$disconnect()),
+
+	saveUserChannels: async (SERVICE, CHANNEL, USERS) =>
+		await prisma.userChannel
+			.createMany({
+				data: USERS.map((USER) => {
+					return {
+						serviceId: SERVICE.id,
+						channelId: CHANNEL.id,
+						userId: USER.id,
+					};
+				}),
+			})
+			.finally(() => prisma.$disconnect()),
+
+	updateChannelName: async (SERVICE, CHANNEL, name) =>
+		await prisma.channel.update({
+			where: { service: SERVICE, id: CHANNEL.id },
+			data: { name: name },
+			select: {
+				service: false,
+				id: true,
+				name: true,
+				type: true,
+			},
+		}),
+
+	deleteChannel: async (SERVICE, USER, channelId) => {
+		const CHANNEL = await prisma.channel
+			.findUnique({
+				where: { service: SERVICE, id: channelId },
+			})
+			.catch(async (err) => {
+				await prisma.$disconnect();
+				if (err.code == "P2020") throw new ChannelException.NotFound();
+				throw err;
+			});
+
+		if (CHANNEL.type.includes("public") && USER.role.id < 9)
+			throw new ChannelException.NotAllowed();
+
+		await prisma.channel.delete({ where: { service: SERVICE, id: channelId } });
+
+		return await prisma.channel.delete({ where: { service: SERVICE, id: CHANNEL } });
 	},
 };
 
