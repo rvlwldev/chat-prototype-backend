2ebc49ac792905d50c5e199ea3e08fb5cdef34e7
rvlwldev <rvlwlasd77@gmail.com>
2023-12-27 10:31:06 +0900

    fix: 스키마 모델 수정 (채널타입, 메세지타입 추가)

--- Configuration/Prisma/Seed.js
+++ Configuration/Prisma/Seed.js
@@ -1,29 +1,48 @@
 const prisma = require("../../Utils/Prisma");
 
 const roleData = [
-	{ code: 99, name: "전체관리자" },
-	{ code: 9, name: "운영자" },
-	{ code: 2, name: "일반사용자" },
 	{ code: 1, name: "익명사용자" },
+	{ code: 2, name: "일반사용자" },
+	{ code: 9, name: "운영자" },
+	{ code: 99, name: "전체관리자" },
 ];
 
-const serviceData = [{ id: "test-service", name: "테스트서비스" }];
+const channelTypeData = [
+	{ code: 10, name: "private" },
+	{ code: 50, name: "public" },
+];
+
+const messageTypeData = [
+	{ code: 10, name: "text" },
+	{ code: 11, name: "image" },
+	{ code: 12, name: "audio" },
+	{ code: 13, name: "video" },
+	{ code: 14, name: "file" },
+	{ code: 15, name: "vote" },
+
+	{ code: 20, name: "notice-created" },
+	{ code: 21, name: "notice-joined" },
+	{ code: 22, name: "notice-left" },
+];
+
+const serviceData = [
+	{ id: "test-service01", name: "테스트서비스01" },
+	{ id: "test-service02", name: "테스트서비스02" },
+	{ id: "test-service03", name: "테스트서비스03" },
+];
 
 const channelData = [
 	{
-		serviceId: "test-service",
-		id: "test-channel-01",
-		name: "테스트채널01",
-		type: "public",
+		serviceId: "test-service01",
+		id: "test-channel01",
+		name: "테스트공개채널01",
+		typeCode: 50,
 	},
-];
-
-const userChannelData = [
 	{
-		serviceId: "test-service",
-		userId: "rlagudwns",
-		channelId: "test-channel-01",
-		readAt: new Date(),
+		serviceId: "test-service01",
+		id: "test-channel02",
+		name: "테스트공개채널02",
+		typeCode: 50,
 	},
 ];
 
@@ -42,9 +61,10 @@ async function seed(initYn = false) {
 		console.log("초기 데이터 저장중...");
 		await prisma.$transaction([
 			prisma.role.createMany({ data: roleData }),
+			prisma.channelType.createMany({ data: channelTypeData }),
+			prisma.messageType.createMany({ data: messageTypeData }),
 			prisma.service.createMany({ data: serviceData }),
 			prisma.channel.createMany({ data: channelData }),
-			prisma.userChannel.createMany({ data: userChannelData }),
 		]);
 	} catch (err) {
 		if (err.code == "P2002") return;
