76c5a71c4a629e2d7f92315d0f9f56327493d376
rvlwldev <rvlwlasd77@gmail.com>
2023-12-27 10:36:42 +0900

    fix: User -> User/Auth 로 파일분기

---
+++ Route/User/Auth.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const ROUTER = express.Router();
+
+const ServiceController = require("../../Controller/Service");
+const UserController = require("../../Controller/User");
+
+const UserExceptions = require("../../Exception/UserException");
+
+const { HttpStatusCode } = require("axios");
+
+ROUTER.post("/register", async (req, res) => {
+	try {
+		const { serviceId, id, password, name, role } = req.body;
+
+		const SERVICE = await ServiceController.getService(serviceId);
+		const USER = await UserController.createUser(SERVICE, id, password, name, role);
+
+		res.status(HttpStatusCode.Created).json({ service: SERVICE, user: USER });
+	} catch (err) {
+		if (UserExceptions.isInstanceOf(err)) res.status(err.httpStatusCode).json(err);
+		else {
+			res.status(HttpStatusCode.InternalServerError).json(err);
+			console.log(err);
+		}
+	}
+});
+
+ROUTER.post("/login", async (req, res) => {
+	try {
+		const SERVICE = await ServiceController.getService(req.body.serviceId);
+		const USER = await UserController.getUser(SERVICE, req.body.user.id);
+		const TOKEN = await UserController.getToken(SERVICE, USER, req.body.user.password);
+
+		res.status(HttpStatusCode.Ok).json({ service: SERVICE, user: USER, token: TOKEN });
+	} catch (err) {
+		if (UserExceptions.isInstanceOf(err)) res.status(err.httpStatusCode).json(err);
+		else {
+			res.status(HttpStatusCode.InternalServerError).json(err);
+			console.log(err);
+		}
+	}
+});
+
+// TODO : 전체 관리자 로그인 (serviceId 없음)
+ROUTER.post("/admin/login", async (req, res) => {});
+
+module.exports = ROUTER;
--- Route/User/User.js
+++ Route/User/User.js
@@ -2,66 +2,32 @@ const express = require("express");
 const ROUTER = express.Router();
 const JWT = require("../../Utils/JWT");
 
-const ServiceController = require("../../Controller/Service");
-const UserController = require("../../Controller/User");
 const ChannelController = require("../../Controller/Channel");
 
 const UserExceptions = require("../../Exception/UserException");
+const ChannelException = require("../../Exception/ChannelException");
 
 const { HttpStatusCode } = require("axios");
 const { WS } = require("../../Utils/WebSocket");
 
-ROUTER.post("/register", async (req, res) => {
-	try {
-		const { serviceId, id, password, name, role } = req.body;
-
-		const SERVICE = await ServiceController.getService(serviceId);
-		const USER = await UserController.createUser(SERVICE, id, password, name, role);
-
-		res.status(HttpStatusCode.Created).json({ service: SERVICE, user: USER });
-	} catch (err) {
-		if (UserExceptions.isInstanceOf(err)) res.status(err.httpStatusCode).json(err);
-		else {
-			res.status(HttpStatusCode.InternalServerError).json(err);
-			console.log(err);
-		}
-	}
-});
-
-ROUTER.post("/login", async (req, res) => {
-	try {
-		const SERVICE = await ServiceController.getService(req.body.serviceId);
-		const USER = await UserController.getUser(SERVICE, req.body.user.id);
-
-		const LOGIN_INFO = await UserController.getToken(SERVICE, USER, req.body.user.password);
-
-		res.status(HttpStatusCode.Ok).json(LOGIN_INFO);
-	} catch (err) {
-		if (UserExceptions.isInstanceOf(err)) res.status(err.httpStatusCode).json(err);
-		else {
-			res.status(HttpStatusCode.InternalServerError).json(err);
-			console.log(err);
-		}
-	}
-});
-
-// TODO : 전체 관리자 로그인 (serviceId 없음)
-ROUTER.post("/admin/login", async (req, res) => {});
-
 ROUTER.get("/:userId/channels", JWT.verify, async (req, res) => {
 	try {
-		const serviceId = req.userData.serviceId;
-		const userId = req.userData.id;
+		const CHANNELS = await ChannelController.getChannelsByUser(req.SERVICE, req.USER);
 
-		const channels = await ChannelController.getChannelsByUserId(serviceId, userId);
+		WS.subscribeAll(
+			req.SERVICE.id,
+			req.USER.id,
+			CHANNELS.map((CHANNEL) => CHANNEL.id)
+		);
 
-		for (const channel of channels) WS.subscribe(serviceId, userId, channel.id);
-
-		res.status(HttpStatusCode.Ok).json({ channels: channels });
+		res.status(HttpStatusCode.Ok).json({ channels: CHANNELS });
 	} catch (err) {
-		if (err instanceof UserNotFoundException)
-			res.status(HttpStatusCode.NotFound).send(err.message);
-		else res.status(HttpStatusCode.InternalServerError).send(err);
+		if (UserExceptions.isInstanceOf(err)) res.status(err.httpStatusCode).json(err);
+		else if (ChannelException.isInstanceOf(err)) res.status(err.httpStatusCode).json(err);
+		else {
+			res.status(HttpStatusCode.InternalServerError).json(err);
+			console.log(err);
+		}
 	}
 });
 
