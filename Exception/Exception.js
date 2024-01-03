391e9bbf172f1fcbe22c4c978c84756d2f45b144
rvlwldev <rvlwlasd77@gmail.com>
2024-01-03 11:38:03 +0900

    refactor: 추상 exception 객체 속성명 변경

--- Exception/Exception.js
+++ Exception/Exception.js
@@ -2,10 +2,10 @@ const { HttpStatusCode } = require("axios");
 
 class Exception extends Error {
 	constructor(message, code) {
-		super(message);
-
-		this.code = code in HttpStatusCode ? code : 500;
+		super();
 		this.error = true;
+		this.httpStatusCode = code in HttpStatusCode ? code : 500;
+		this.message = message;
 	}
 }
 
